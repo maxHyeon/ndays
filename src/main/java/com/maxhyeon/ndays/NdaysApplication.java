4510c059e700175d5d6efd86b95bd0a375fcf767
maxHyeon <americano314@gmail.com>
2019-12-24 15:07:50 +0900

    Initial commit

---
+++ src/main/java/com/maxhyeon/ndays/NdaysApplication.java
@@ -0,0 +1,13 @@
+package com.maxhyeon.ndays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NdaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxhyeon/ndays/NdaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxhyeon.ndays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
