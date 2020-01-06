248f42ee378a177514cac6a02a435f2f84966464
maxHyeon <americano314@gmail.com>
2020-01-06 23:41:20 +0900

    crud 구현 delete는 response 에 500 떨어지는 부분 수정 필요

--- src/main/java/com/maxhyeon/ndays/v1/UserController.java
+++ src/main/java/com/maxhyeon/ndays/v1/UserController.java
@@ -1,6 +1,7 @@
 package com.maxhyeon.ndays.v1;
 
 import com.maxhyeon.ndays.entity.Users;
+import com.maxhyeon.ndays.model.response.CommonResult;
 import com.maxhyeon.ndays.model.response.ListResult;
 import com.maxhyeon.ndays.model.response.SingleResult;
 import com.maxhyeon.ndays.repo.UserJpaRepo;
@@ -24,6 +25,7 @@ import java.util.List;
 public class UserController {
 
     private final UserJpaRepo userJpaRepo;
+
     private final ResponseService responseService; // 결과를 처리할 Service
     @Operation(summary  = "회원 리스트 조회", description = "모든 회원을 조회한다",tags={"user"})
     @GetMapping(value = "/users")
@@ -40,32 +42,31 @@ public class UserController {
 
     @Operation(summary  = "회원 입력", description = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public Users save(@Parameter(description = "회원아이디", required = true) @RequestParam(value="page", defaultValue="1") String uid,
+    public SingleResult<Users> save(@Parameter(description = "회원아이디", required = true) @RequestParam(value="page", defaultValue="1") String uid,
                      @Parameter(description = "회원이름", required = true) @RequestParam String name) {
-        Users user = Users.builder()
+        Users users = Users.builder()
                 .uid(uid)
                 .name(name)
                 .build();
-        return userJpaRepo.save(user);
+        return responseService.getSingleResult(userJpaRepo.save(users));
     }
-//    @Operation(summary  = "회원 입력", description = "회원을 입력한다.")
-//    @PostMapping(value = "/user")
-//    public Users save(@Parameter(description = "회원아이디", required = true) @RequestParam(value="page", defaultValue="1") String uid,
-//                      @Parameter(description = "회원이름", required = true) @RequestParam String name) {
-//        Users user = Users.builder()
-//                .uid(uid)
-//                .name(name)
-//                .build();
-//        return userJpaRepo.save(user);
-//    }
-//    @Operation(summary  = "회원 입력", description = "회원을 입력한다.")
-//    @PostMapping(value = "/user")
-//    public Users save(@Parameter(description = "회원아이디", required = true) @RequestParam(value="page", defaultValue="1") String uid,
-//                      @Parameter(description = "회원이름", required = true) @RequestParam String name) {
-//        Users user = Users.builder()
-//                .uid(uid)
-//                .name(name)
-//                .build();
-//        return userJpaRepo.save(user);
-//    }
+    @Operation(summary  = "회원 수정", description = "회원을 수정한다.")
+    @PutMapping(value = "/user")
+    public SingleResult<Users> modify(
+                      @Parameter(description = "회원번호", required = true) @RequestParam(value="page", defaultValue="1") long msrl,
+                      @Parameter(description = "회원ID", required = true) @RequestParam String uid,
+                      @Parameter(description = "회원이름", required = true) @RequestParam String name){
+        Users users = Users.builder()
+                .msrl(msrl)
+                .uid(uid)
+                .name(name)
+                .build();
+        return responseService.getSingleResult(userJpaRepo.save(users));
+    }
+    @Operation(summary  = "회원 삭제", description = "회원번호로 회원을 삭제한다.")
+    @DeleteMapping(value = "/user/{msrl}")
+    public CommonResult delete(@Parameter(description = "회원번호", required = true) @PathVariable(value = "1") long msrl) {
+        userJpaRepo.deleteById(msrl);
+        return responseService.getSuccessResult();
+    }
 }
